e12dbf5445e325c18ab2c7975f2e7969588d4864
Brian Mearns <mearns.b@gmail.com>
2017-11-21 22:29:08 -0500

    MOre smarts for storage property

--- src/web/routers/api-router.js
+++ src/web/routers/api-router.js
@@ -34,6 +34,26 @@ function getGraphQLSchema () {
   return fs.readFile(path.resolve(__dirname, 'schema.graphql'), 'utf8')
 }
 
+const STORAGE_UNIT_SCALE_VALUES = {
+  B: 1,
+  KB: 1e3,
+  MG: 1e6,
+  GB: 1e9,
+  TB: 1e12,
+  PB: 1e15,
+  EB: 1e18,
+  ZB: 1e21,
+  YB: 1e24,
+  KiB: 1024,
+  MiB: 1024 * 1024,
+  GiB: 1024 * 1024 * 1024,
+  TiB: 1024 * 1024 * 1024 * 1024,
+  PiB: 1024 * 1024 * 1024 * 1024 * 1024,
+  EiB: 1024 * 1024 * 1024 * 1024 * 1024 * 1024,
+  ZiB: 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024,
+  YiB: 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024
+}
+
 function getGraphQLResolverMap ({poolCollection}) {
   return {
     Query: {
@@ -65,7 +85,7 @@ function getGraphQLResolverMap ({poolCollection}) {
       }
     },
     Property: {
-      __resolveType: ({name, value}) => {
+      __resolveType: ({name, value}, context, info) => {
         switch (typeof value) {
           case 'undefined':
             return 'NullProperty'
@@ -82,7 +102,10 @@ function getGraphQLResolverMap ({poolCollection}) {
             if (value === null) {
               return 'NullProperty'
             } else if (value.hasOwnProperty('value') && value.hasOwnProperty('unit')) {
-              return 'StorageSizeProperty'
+              const storageUnitType = info.schema.getType('StorageUnit')
+              if (typeof value.value === 'number' && storageUnitType.getValues().some(({name}) => name === value.unit)) {
+                return 'StorageSizeProperty'
+              }
             }
         }
         return null
@@ -124,6 +147,12 @@ function getGraphQLResolverMap ({poolCollection}) {
       value: () => null,
       valueType: (obj, args, context, info) => info.schema.getType('String')
     },
-    StorageSize: {}
+    StorageSize: {
+      scale: ({unit}) => STORAGE_UNIT_SCALE_VALUES[unit],
+      str: ({value, unit}) => `${value}${unit}`,
+      bytes: ({value, unit}) => {
+        return STORAGE_UNIT_SCALE_VALUES[unit] * value
+      }
+    }
   }
 }
