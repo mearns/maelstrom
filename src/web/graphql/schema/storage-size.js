f73a0330b66d585f26d4870dcd21f05a809a961f
Brian Mearns <mearns.b@gmail.com>
2017-11-24 14:50:37 -0500

    Add more unit tests for storage size schema
    [ci skip]

--- src/web/graphql/schema/storage-size.js
+++ src/web/graphql/schema/storage-size.js
@@ -1,23 +1,24 @@
 import {loadSchema} from '../schema-tools'
+import Promise from 'bluebird'
 
-const STORAGE_UNIT_SCALE_VALUES = {
-  U: 1,
-  K: 1e3,
-  M: 1e6,
-  G: 1e9,
-  T: 1e12,
-  P: 1e15,
-  E: 1e18,
-  Z: 1e21,
-  Y: 1e24,
-  Ki: 1024,
-  Mi: 1024 * 1024,
-  Gi: 1024 * 1024 * 1024,
-  Ti: 1024 * 1024 * 1024 * 1024,
-  Pi: 1024 * 1024 * 1024 * 1024 * 1024,
-  Ei: 1024 * 1024 * 1024 * 1024 * 1024 * 1024,
-  Zi: 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024,
-  Yi: 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024
+const MULTIPLIERS = {
+  U: {scale: 1, description: 'A single unit'},
+  K: {scale: 1e3, description: 'A kilo-unit = 1,000 (thousand) units'},
+  M: {scale: 1e6, description: 'A mega-unit = 1,000,000 (million) units'},
+  G: {scale: 1e9, description: 'A giga-unit = 1e9 (billion) units'},
+  T: {scale: 1e12, description: 'A tera-unit = 1e12 (trillion) units'},
+  P: {scale: 1e15, description: 'A peta-unit = 1e15 (quadrillion) units'},
+  E: {scale: 1e18, description: 'An exa-unit = 1e18 units'},
+  Z: {scale: 1e21, description: 'A zetta-unit = 1e21 units'},
+  Y: {scale: 1e24, description: 'A yotta-unit = 1e24 units'},
+  Ki: {scale: 1024, description: 'A kibi-unit = 2^10 units (1024)'},
+  Mi: {scale: 1024 * 1024, description: 'A mebi-unit = 2^20 units (1024*1024)'},
+  Gi: {scale: 1024 * 1024 * 1024, description: 'A gibi-unit = 2^30 units (1024^3)'},
+  Ti: {scale: 1024 * 1024 * 1024 * 1024, description: 'A tebi-unit= 2^40 units'},
+  Pi: {scale: 1024 * 1024 * 1024 * 1024 * 1024, description: 'A pebi-unit = 2^50 units'},
+  Ei: {scale: 1024 * 1024 * 1024 * 1024 * 1024 * 1024, description: 'An exbi-unit = 2^60 units'},
+  Zi: {scale: 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024, description: 'A zebi-unit = 2^70 units'},
+  Yi: {scale: 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024, description: 'A yobi-unit = 2^80 units'}
 }
 
 export class StorageSize {
@@ -28,7 +29,7 @@ export class StorageSize {
       ? unit.substring(0, unit.length - 1)
       : unit
     this.unit = strippedUnit.length === 0 ? 'U' : strippedUnit
-    this.scale = STORAGE_UNIT_SCALE_VALUES[this.unit]
+    this.scale = MULTIPLIERS[this.unit].scale
     this.str = `${this.value}${this.unit === 'U' ? '' : this.unit}B`
     this.bytes = this.scale * this.value
   }
@@ -39,5 +40,19 @@ export function getResolverMap () {
 }
 
 export function getTypeDefs () {
-  return loadSchema('storage-size') // TODO: XXX: Auto-generate the MultiplierPrefix type from the map above.
+  return Promise.join(
+    loadSchema('storage-size'),
+    getMultiplierPrefixTypeDef(),
+    (schema1, schema2) => `${schema1}\n${schema2}`
+  )
+}
+
+function getMultiplierPrefixTypeDef () {
+  return `
+    enum MultiplierPrefix {
+      ${Object.keys(MULTIPLIERS).map((prefix) => {
+        const {description} = MULTIPLIERS[prefix]
+        return `  # ${description}\n  ${prefix}\n`
+      }).join('\n')}
+    }`
 }
--- test/src/web/graphql/schema/storage-size.spec.js
+++ test/src/web/graphql/schema/storage-size.spec.js
@@ -12,35 +12,183 @@ import {graphql} from 'graphql'
 import Promise from 'bluebird'
 
 describe('storage-size schema module', () => {
-  describe('type MultiplierPrefix', () => {
-    it('should do something', () => {
-      // given
+
+  function runQuery ({mocks = {}, queryTypeDef, query}) {
+    return Promise.resolve(getExecutableSchema(
+      [getTypeDefs(), queryTypeDef],
+      getResolverMap()
+    ))
+      .then((schema) => {
+        addMockFunctionsToSchema({
+          schema, mocks, preserveResolvers: true
+        })
+        return graphql(schema, query)
+      })
+  }
+
+  it('should have all the expected fields', () => {
+    // given
+    const mocks = {
+      StorageSize: () => new StorageSize({
+        value: 12345,
+        unit: 'Mi'
+      })
+    }
+
+    return runQuery({
+      mocks,
+      queryTypeDef: `type Query {testQuery: StorageSize}`,
+      // when
+      query: `{testQuery {value, unit, scale, str, bytes}}`
+    })
+      // then
+      .then(({data: {testQuery}}) => {
+        expect(testQuery.value).to.equal(12345)
+        expect(testQuery.unit).to.equal('Mi')
+        expect(testQuery.scale).to.equal(1024 * 1024)
+        expect(testQuery.str).to.equal('12345MiB')
+        expect(testQuery.bytes).to.equal(12345 * 1024 * 1024)
+      })
+  })
+
+  it('should support units with the "B" suffix', () => {
+    // given
+    const mocks = {
+      StorageSize: () => new StorageSize({
+        value: 314,
+        unit: 'MB'
+      })
+    }
+
+    return runQuery({
+      mocks,
+      queryTypeDef: `type Query {testQuery: StorageSize}`,
+      // when
+      query: `{testQuery {unit, scale, str, bytes}}`
+    })
+      // then
+      .then(({data: {testQuery}}) => {
+        expect(testQuery.unit).to.equal('M')
+        expect(testQuery.scale).to.equal(1e6)
+        expect(testQuery.str).to.equal('314MB')
+        expect(testQuery.bytes).to.equal(314000000)
+      })
+  })
+
+  it('should support "U" as the unit', () => {
+    // given
+    const mocks = {
+      StorageSize: () => new StorageSize({
+        value: 314,
+        unit: 'U'
+      })
+    }
+
+    return runQuery({
+      mocks,
+      queryTypeDef: `type Query {testQuery: StorageSize}`,
+      // when
+      query: `{testQuery {unit, scale, str, bytes}}`
+    })
+      // then
+      .then(({data: {testQuery}}) => {
+        expect(testQuery.unit).to.equal('U')
+        expect(testQuery.scale).to.equal(1)
+        expect(testQuery.str).to.equal('314B')
+        expect(testQuery.bytes).to.equal(314)
+      })
+  })
+
+  it('should support "B" as the unit', () => {
+    // given
+    const mocks = {
+      StorageSize: () => new StorageSize({
+        value: 1717,
+        unit: 'B'
+      })
+    }
+
+    return runQuery({
+      mocks,
+      queryTypeDef: `type Query {testQuery: StorageSize}`,
+      // when
+      query: `{testQuery {unit, scale, str, bytes}}`
+    })
+      // then
+      .then(({data: {testQuery}}) => {
+        expect(testQuery.unit).to.equal('U')
+        expect(testQuery.scale).to.equal(1)
+        expect(testQuery.str).to.equal('1717B')
+        expect(testQuery.bytes).to.equal(1717)
+      })
+  })
+
+  it('should support empty-string as the unit', () => {
+    // given
+    const mocks = {
+      StorageSize: () => new StorageSize({
+        value: 866,
+        unit: ''
+      })
+    }
+
+    return runQuery({
+      mocks,
+      queryTypeDef: `type Query {testQuery: StorageSize}`,
+      // when
+      query: `{testQuery {unit, scale, str, bytes}}`
+    })
+      // then
+      .then(({data: {testQuery}}) => {
+        expect(testQuery.unit).to.equal('U')
+        expect(testQuery.scale).to.equal(1)
+        expect(testQuery.str).to.equal('866B')
+        expect(testQuery.bytes).to.equal(866)
+      })
+  })
+
+  it('should support all the expected multiplier prefixes', () => {
+    // given
+    const expectedUnits = {
+      U: 1,
+      K: 1e3,
+      M: 1e6,
+      G: 1e9,
+      T: 1e12,
+      P: 1e15,
+      E: 1e18,
+      Z: 1e21,
+      Y: 1e24,
+      Ki: 1024,
+      Mi: 1024 * 1024,
+      Gi: 1024 * 1024 * 1024,
+      Ti: 1024 * 1024 * 1024 * 1024,
+      Pi: 1024 * 1024 * 1024 * 1024 * 1024,
+      Ei: 1024 * 1024 * 1024 * 1024 * 1024 * 1024,
+      Zi: 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024,
+      Yi: 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024
+    }
+
+    return Promise.map(Object.keys(expectedUnits), (prefix) => {
+      const expectedScale = expectedUnits[prefix]
       const mocks = {
         StorageSize: () => new StorageSize({
-          value: 12345,
-          unit: 'Mi'
+          value: 123,
+          unit: prefix
         })
       }
 
-      return Promise.resolve(getExecutableSchema(
-        [getTypeDefs(), `type Query {testQuery: StorageSize}`],
-        getResolverMap())
-      )
-        .then((executableSchema) => {
-          addMockFunctionsToSchema({schema: executableSchema, mocks, preserveResolvers: true})
-          return graphql(executableSchema, `
-{ testQuery { value, unit, scale, str, bytes } }
-          `)
-        })
-        .tap((response) => {
-          console.log(response)
-        })
+      return runQuery({
+        mocks,
+        queryTypeDef: `type Query {testQuery: StorageSize}`,
+        // when
+        query: `{testQuery {unit, scale, str, bytes}}`
+      })
+        // then
         .then(({data: {testQuery}}) => {
-          expect(testQuery.value).to.equal(12345)
-          expect(testQuery.unit).to.equal('Mi')
-          expect(testQuery.scale).to.equal(1024 * 1024)
-          expect(testQuery.str).to.equal('12345MiB')
-          expect(testQuery.bytes).to.equal(12345 * 1024 * 1024)
+          expect(testQuery.unit, `Expected unit to match for ${prefix}`).to.equal(prefix)
+          expect(testQuery.scale, `Expected scale to match for ${prefix}`).to.equal(expectedScale)
+          expect(testQuery.bytes, `Expected bytes to match for ${prefix}`).to.equal(123 * expectedScale)
         })
     })
   })
